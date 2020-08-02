2aabff4c5b1c9e1a2812777cc8816ff2f7b55a7f
Rahul Rao <RAOR378647@exactsoftware.com>
2020-08-02 19:50:26 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,63 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-nested-form-array-app';
+
+  empForm: FormGroup;
+
+
+  constructor(private fb: FormBuilder) {
+
+    this.empForm = this.fb.group({
+      employees: this.fb.array([]),
+    })
+  }
+
+  //getter for employees
+  employees(): FormArray {
+    return this.empForm.get("employees") as FormArray
+  }
+
+
+  newEmployee(): FormGroup {
+    return this.fb.group({
+      firstName: '',
+      lastName: '',
+      skills: this.fb.array([])
+    })
+  }
+
+
+  addEmployee() {
+    console.log("Adding an employee");
+    this.employees().push(this.newEmployee());
+  }
+
+
+  removeEmployee(empIndex: number) {
+    this.employees().removeAt(empIndex);
+  }
+
+  //getter for skills
+  employeeSkills(empIndex: number): FormArray {
+    return this.employees().at(empIndex).get("skills") as FormArray
+  }
+
+  newSkill(): FormGroup {
+    return this.fb.group({
+      skill: '',
+      exp: '',
+    })
+  }
+
+  addEmployeeSkill(empIndex: number) {
+    this.employeeSkills(empIndex).push(this.newSkill());
+  }
+
+  removeEmployeeSkill(empIndex: number, skillIndex: number) {
+    this.employeeSkills(empIndex).removeAt(skillIndex);
+  }
+
+  onSubmit() {
+    console.log(this.empForm.value);
+  }
 }
